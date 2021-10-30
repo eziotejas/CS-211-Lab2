eb90b5dbfc0b2401e19a8d7bf8bc021259853f2c
tejas shivanand mane <tmane@seas.upenn.edu>
2021-10-29 21:03:49 -0700

    dsad

--- src/for_you_to_do.c
+++ src/for_you_to_do.c
@@ -74,6 +74,8 @@ int mydgetrf(double *A, int *ipiv, int n)
                                 {
                                         A[max_ind*n+temp_iter] = tempv[temp_iter];
                                 }
+
+                                free(tempv);
                         }
 
 
@@ -199,7 +201,7 @@ void mydgemm(double *A, double *B, double *C, int n, int i, int j, int k, int b)
     /* add your code here */
     /* please just copy from your lab1 function optimal( ... ) */
 
-    
+
 
 
 
