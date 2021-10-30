148608bd9cb5b3aa84386ea1bae91bd4a18b470f
eziotejas <ezioauditoretejas@gmail.com>
2021-10-29 20:08:37 -0700

    Update for_you_to_do.c

--- src/for_you_to_do.c
+++ src/for_you_to_do.c
@@ -3,7 +3,7 @@
 int get_block_size(){
     //return the block size you'd like to use 
     /*add your code here */
-    return 128;
+    return 2;
   
 }
 
@@ -26,12 +26,73 @@ int get_block_size(){
  **/
 
 
-int mydgetrf(double *A, int *ipiv, int n) 
+int mydgetrf(double *A, int *ipiv, int n)
 {
     /* add your code here */
 
+        int i = 0, t = 1, j, k;
+        for (i = 0; i < n-1;i++)
+        {
+                int max_ind = i;
+                int max = abs(A[i,i]);
+                for (t = i+1; t < n; t ++)
+                {
+                        if (abs(A[t,i]) >max )
+                        {
+                                max_ind = t;
+                                max = abs(A[t,i]);
+                        }
+
+                }
+
+                if (max==0)
+                {
+                        return -1;
+                }
+                else
+                {
+                        if (max_ind!=i)
+                        {
+                                double temps;
+                                temps = ipiv[i];
+                                ipiv[i] = ipiv[max_ind];
+                                ipiv[max_ind] = temps;
+                                double *tempv = (double *) malloc(sizeof(double) * n);
+                                int temp_iter = 0;
+                                for (temp_iter = 0; temp_iter<n; temp_iter++)
+                                {
+                                        tempv[temp_iter] = A[i*n+temp_iter];
+                                }
+
+                                for (temp_iter = 0; temp_iter<n; temp_iter++)
+                                {
+                                        A[i*n+temp_iter] = A[max_ind*n+temp_iter];
+                                }
+
+
+                                for (temp_iter = 0; temp_iter<n; temp_iter++)
+                                {
+                                        A[max_ind*n+temp_iter] = tempv[temp_iter];
+                                }
+                        }
+
+
+                }
+
+                for (j = i+1;j < n; j++)
+                {
+                        A[j*n+i] = A[j*n+i] / A[i*n+i];
+                        for (k = i+1; k < n;k++)
+                        {
+                                A[j*n+k] = A[j*n+k] - A[j*n+i]*A[i*n+k];
+                        }
+                }
+        }
+
+
     return 0;
 }
+
 
 /**
  * 
@@ -62,7 +123,69 @@ int mydgetrf(double *A, int *ipiv, int n)
  **/
 void mydtrsv(char UPLO, double *A, double *B, int n, int *ipiv)
 {
+
+        y = (double *) malloc(sizeof(double) * n);
+        x = (double *) malloc(sizeof(double) * n);
+
+        int i, j;
+        double temp = 0;
+
     /* add your code here */
+        if (UPLO == 'L')
+        {
+                y[0] = B[ipiv[0]];
+
+                for (i = 1; i < n; i++)
+                {
+                        y[i] = B[ipiv[i]];
+
+                        for (j = 1; j < i; j++) 
+                        {
+                                y[i] -= y[j] * A[i*n + j]; 
+                        }
+
+                }
+
+
+                for (i = 1; i < n; i++)
+                {
+                       B[i] = y[i];
+                }
+
+        }
+
+        else
+        {
+                
+                for (i = 1; i < n; i++)
+                {
+                       y[i] = B[i];
+                }
+
+                x[n-1] = y[n-1] / A[n*n-1] ;
+
+                for (i = n-2; i > 0; i--)
+                {
+                        x[i] = y[i];
+
+                        for (j = i+1; j < n; j++) 
+                        {
+                                x[i] -= (x[j] * A[i*n + j] )/ A[i*n+i] ; 
+                        }
+                }
+
+
+                for (i = 1; i < n; i++)
+                {
+                       B[i] = x[i];
+                }
+
+                
+        }
+        free(y);
+        free(x);
+
+
     return;
 }
 
