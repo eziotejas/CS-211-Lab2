42839cce828ca4434efbf5191f35a5059d4da7cf
tejas shivanand mane <tmane@seas.upenn.edu>
2021-10-29 23:39:03 -0700

    dsad

--- src/for_you_to_do.c
+++ src/for_you_to_do.c
@@ -190,7 +190,7 @@ void mydtrsv(char UPLO, double *A, double *B, int n, int *ipiv)
                 }
 
 
-                for (i = 1; i < n; i++)
+                for (i = 0; i < n; i++)
                 {
                        B[i] = y[i];
                 }
