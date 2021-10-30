0632012d504430baacdbe27eb0cfd2b239b7f89b
tejas shivanand mane <tmane@seas.upenn.edu>
2021-10-30 01:57:36 -0700

    dsa

--- test/verification.c
+++ test/verification.c
@@ -23,7 +23,7 @@
 int test_all_lu_functions()
 {
     int block_size=get_block_size();
-    int ni, nList[] = {5};
+    int ni, nList[] = {1000, 2000, 3000, 4000, 5000};
     for (ni = 0; ni < sizeof(nList) / sizeof(nList[0]); ni++) {
         int n = nList[ni], i, j;
 
@@ -54,7 +54,6 @@ int test_all_lu_functions()
         if ( matrix_copy(B2, B1, n, 1) ) return -1;
         if ( matrix_copy(B3, B1, n, 1) ) return -1;
 
-
         t0 = get_sec();    
         lapack_lu(A1, B1, n);
         t1 = get_sec();
@@ -63,23 +62,18 @@ int test_all_lu_functions()
         t0 = get_sec();    
         naive_lu(A2, B2, n);
         t1 = get_sec();
-
-
         printf("Elapsed time, naive LU: %lf seconds\n", t1 - t0);
 
         if (verify_matrix(A1, A2, n, n) || verify_matrix(B1, B2, n, 1))
-        {
             printf("my naive LU is incorrect.\n");
-            
-        }
 
-        // t0 = get_sec();    
-        // block_lu(A3, B3, n,  block_size);
-        // t1 = get_sec();
-        // printf("Elapsed time, block LU: %lf seconds\n", t1 - t0);
+        t0 = get_sec();    
+        block_lu(A3, B3, n,  block_size);
+        t1 = get_sec();
+        printf("Elapsed time, block LU: %lf seconds\n", t1 - t0);
 
-        // if (verify_matrix(A1, A3, n, n) || verify_matrix(B1, B3, n, 1))
-        //     printf("my block LU is incorrect.\n");
+        if (verify_matrix(A1, A3, n, n) || verify_matrix(B1, B3, n, 1))
+            printf("my block LU is incorrect.\n");
 
         free(A1);
         free(A2);
