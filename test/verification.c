6d54fc3c7259eebcededfba89330b8b1f0a32708
tejas shivanand mane <tmane@seas.upenn.edu>
2021-10-30 02:29:09 -0700

    gh

--- test/verification.c
+++ test/verification.c
@@ -67,13 +67,32 @@ int test_all_lu_functions()
         if (verify_matrix(A1, A2, n, n) || verify_matrix(B1, B2, n, 1))
             printf("my naive LU is incorrect.\n");
 
-        t0 = get_sec();    
-        block_lu(A3, B3, n,  block_size);
-        t1 = get_sec();
-        printf("Elapsed time, block LU: %lf seconds\n", t1 - t0);
 
-        if (verify_matrix(A1, A3, n, n) || verify_matrix(B1, B3, n, 1))
-            printf("my block LU is incorrect.\n");
+
+
+
+
+
+
+
+        printf("\n\n A1 is \n\n");
+        print_matrix(A1, n,n);
+        printf("\n\n A2 is \n\n");
+        print_matrix(A2, n,n);
+
+        printf("\n\n B1 is \n\n");
+        print_matrix(B1, n,1);
+
+        printf("\n\n B2 is \n\n");
+        print_matrix(B2, n, 1);
+
+        // t0 = get_sec();    
+        // block_lu(A3, B3, n,  block_size);
+        // t1 = get_sec();
+        // printf("Elapsed time, block LU: %lf seconds\n", t1 - t0);
+
+        // if (verify_matrix(A1, A3, n, n) || verify_matrix(B1, B3, n, 1))
+        //     printf("my block LU is incorrect.\n");
 
         free(A1);
         free(A2);
