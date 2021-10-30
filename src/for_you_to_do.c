70de2afa20d72e2e41c0eb0db64457977104692a
tejas shivanand mane <tmane@seas.upenn.edu>
2021-10-30 01:23:26 -0700

    dsad

--- src/for_you_to_do.c
+++ src/for_you_to_do.c
@@ -229,7 +229,7 @@ void mydtrsv(char UPLO, double *A, double *B, int n, int *ipiv)
                 {
                         x[i] = y[i];
 
-                        for (j = i+1; j >=0; j--) 
+                        for (j = i+1; j >0; j--) 
                         {
                                 printf("\n Atemp[i*n + j]is %lf, x[j] is %lf \n", Atemp[i*n + j], x[j] );
                                 printf("\n Pre x[i] is %lf \n",x[i] );
